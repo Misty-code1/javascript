45ffa52b40614f28a052da8552023053b8daf6eb
Bright Anyasi <anyasibright@gmail.com>
2023-03-01 13:36:30 +0100

    worked on variables and operators

---
+++ app.js
@@ -0,0 +1,97 @@
+/*
+1. Variables (completed)
+2. operators and operations
+3. conditionals
+4. display
+5. functions
+6. loops
+7. string methods
+8. number methods
+9. Array methods
+10. DOM Manipulation
+11. Objects
+12. Dates
+
+Projects
+1. Todo List
+2. Calculator
+3. Result checker
+4. Dictionary
+5. 
+
+*************Variable types*****************
+1. string
+2. number: 1234
+3. Boolean: false or true
+4. array: ["charles", "Bright", 25, true]
+5. object: {} 
+user = {
+    name: "Charge",
+    age: 25
+}
+
+car = {
+    year: 2015,
+    model: "jjfh"
+}
+
+6. undefined
+7. NaN 
+8. Null
+
+************variable Declaration*******************
+keywords (var, let & const)
+variable name (variableName) charles01
+assignment operator =
+value(variable type) 
+*/
+let firstName = "Charles Charles"
+let age = 20
+console.log(age)
+let isPaid = true
+let names = ['Charles', "Bright", "victor"]
+let school
+let car = {
+    model: "fsffdfdf",
+    year: 2025
+}
+age = 50
+console.log(isPaid)
+console.log(age)
+console.log(car.year)
+console.log(firstName + " is " + age + " years old")
+
+/*
+*****************Operators***************
+plus        +
+minus       -
+multiply    *
+divide      /
+power       **
+modulus     %
+equal to    == or ===
+not         !
+not equals  != or !==
+greater than    >
+less than <
+less than or equals to <=
+greater than or equals to >=
+and &&
+or ||
+
+*/
+
+
+let num1 = 50
+let num2 = 20
+
+console.log(num1 + num2)
+console.log(num1 - num2)
+console.log(num1 * num2)
+console.log(num1 / num2)
+console.log(num1 % num2)
+console.log(num1**2)
+console.log(num1 > num2)
+console.log(num1 < num2)
+console.log(num1 < num2 || num1 - num2 > 25)
+console.log(num1 < num2 && num1 - num2 > 25)
